7134c50b8f5985659b46717c122111253f37a8ca
Rober <robersanchmeg@gmail.com>
2020-04-21 20:51:29 +0200

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -11,4 +11,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ===============================
 // Base de datos
 // ===============================
-process.env.URLDB = process.env.NODE_ENV == 'dev' ? 'mongodb://localhost:27017/cafe' : 'mongodb+srv://strider:FpE45ri9nSldOEfY@cluster0-em3uv.mongodb.net/cafe?retryWrites=true&w=majority';
+process.env.URLDB = process.env.NODE_ENV == 'dev' ? 'mongodb://localhost:27017/cafe' : `mongodb+srv://strider:${process.env.MONGO_URI}@cluster0-em3uv.mongodb.net/cafe?retryWrites=true&w=majority`;
