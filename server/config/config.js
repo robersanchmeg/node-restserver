9de2bb6af299a5f14722e93b4d92cbd78bbf3bfd
Rober <robersanchmeg@gmail.com>
2020-05-03 14:59:05 +0200

    Google Sing-In

--- server/config/config.js
+++ server/config/config.js
@@ -26,3 +26,8 @@ process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
 // Base de datos
 // ===============================
 process.env.URLDB = process.env.NODE_ENV == 'dev' ? 'mongodb://localhost:27017/cafe' : `mongodb+srv://strider:${process.env.MONGO_URI}@cluster0-em3uv.mongodb.net/cafe?retryWrites=true&w=majority`;
+
+// ===============================
+// Google Client ID
+// ===============================
+process.env.CLIENT_ID = process.env.CLIENT_ID || '123146070513-mpssk2tqjs8rb05j3lgvom1svkrvoigl.apps.googleusercontent.com';
